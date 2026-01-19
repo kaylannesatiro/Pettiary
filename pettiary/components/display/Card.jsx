89913132347c7580b52d25b251c130df4d0c330a
carlacristina130523@gmail.com <carlacristina130523@gmail.com>
2026-01-19 11:26:14 -0300

    feat: adicionar placeholder de imagem ao Card

--- pettiary/components/display/Card.jsx
+++ pettiary/components/display/Card.jsx
@@ -4,6 +4,14 @@ import { View, Text, StyleSheet } from 'react-native';
 const Card = ({ pet }) => {
   return (
     <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <View style={[styles.image, styles.placeholderContainer]}>
+          <Text style={styles.placeholderText}>
+            {pet.type === 'cat' ? '🐱' : '🐶'}
+          </Text>
+        </View>
+      </View>
+      
       <Text style={styles.name}>{pet.name}</Text>
       <Text>{pet.gender} • {pet.age}</Text>
     </View>
@@ -17,6 +25,28 @@ const styles = StyleSheet.create({
     padding: 20,
     marginHorizontal: 20,
     marginVertical: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 3 },
+    shadowOpacity: 0.2,
+    shadowRadius: 8,
+    elevation: 5,
+  },
+  imageContainer: {
+    width: '100%',
+    marginBottom: 14,
+  },
+  image: {
+    width: '100%',
+    height: 240,
+    borderRadius: 16,
+  },
+  placeholderContainer: {
+    backgroundColor: '#E1D8CF',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  placeholderText: {
+    fontSize: 80,
   },
   name: {
     fontSize: 24,
