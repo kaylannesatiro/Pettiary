adea372ac0b94590977807f542f38c2e9d56739e
carlacristina130523@gmail.com <carlacristina130523@gmail.com>
2026-01-19 10:52:18 -0300

    feat: criar estrutura básica da tela de animais cadastrados

---
+++ pettiary/components/screen/RegistredPetsScreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { usePets } from '../contexts/PetsContext';
+
+const RegisteredPetsScreen = () => {
+  return (
+    <View>
+      <Text>Animais Cadastrados</Text>
+    </View>
+  );
+};
+
+export default RegisteredPetsScreen;
