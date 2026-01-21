c6d888e2752c853f7b70e20d2dd77496c5fe90ff
carlacristina130523@gmail.com <carlacristina130523@gmail.com>
2026-01-21 08:24:01 -0300

    refactor: remove campo birthDate

--- pettiary/backend/models/Pet.js
+++ pettiary/backend/models/Pet.js
@@ -1,34 +1,21 @@
 const { v4: uuidv4 } = require('uuid');
 
+
 class Pet {
-    constructor(name, species, breed, birthDate, photoUrl, color = '#6200EE') {
+    constructor(name, species, breed, photoUrl, color = '#6200EE') {
         this.id = uuidv4();
         this.name = name;
-        this.species = species; 
+        this.species = species;
         this.breed = breed;
-        this.birthDate = birthDate;
         this.photoUrl = photoUrl;
         this.color = color;
         this.createdAt = new Date();
         this.updatedAt = new Date();
     }
 
-    getAge() {
-        const today = new Date();
-        const birth = new Date(this.birthDate);
-        let age = today.getFullYear() - birth.getFullYear();
-        const monthDiff = today.getMonth() - birth.getMonth();
-        
-        if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birth.getDate())) {
-            age--;
-        }
-        
-        return age;
-    }
-
     update(data) {
         Object.keys(data).forEach(key => {
-            if (this.hasOwnProperty(key) && key !== 'id' && key !== 'createdAt') {
+            if (this.hasOwnProperty(key) && key !== 'id' && key !== 'createdAt' && key !== 'birthDate') {
                 this[key] = data[key];
             }
         });
